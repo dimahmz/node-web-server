24ec6624591ae62faf87ff3cb7bdde4b71dbbfb7
dimahmz <essaadihamza4@outlook.com>
2023-05-15 13:26:30 +0100

    test

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-const http= require('http');
-const fs =require('fs');
+const http = require("http");
+const fs = require("fs");
 const port = 7838;
+const addresses = require("./ip");
 
-const server =http.createServer((req, res) => {
-if(req.url==='/'){
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile("./index.html", (error, html) => {
+      if (error) {
+        res.write(error);
+        res.end();
+      } else {
+        res.write(html);
+        res.end();
+      }
+    });
+    return;
+  }
 
-	fs.readFile('./index.html', (error, html)=>{
+  if (!req.url.startsWith("/files/")) {
+    res.write("this file doesn't exist");
+    res.end();
+    return;
+  }
 
-	if(error) { res.write(error); res.end();}
-	else {res.write(html);res.end();}});
-	return;
-}
+  fs.readFile("./" + req.url, (error, file) => {
+    if (error) {
+      res.write("this file doesn't exist");
+      res.end();
+      console.log(error);
+    } else {
+      res.write(file);
+      res.end();
+    }
+  });
 
-if(!req.url.startsWith("/files/")) {
-	res.write("this file doesn't exist");res.end();
-	return
-};
-
-fs.readFile('./'+req.url, (error, book)=>{
-if(error) {res.write("this file doesn't exist");res.end();console.log(error);}
-else {res.write(book);res.end();}});
-
-// console.log("new connection");
+  // console.log("new connection");
 });
 
 server.listen(port);
 
 console.log(`server is running on ${port} port`);
+console.log("ip addresses : " + addresses);
